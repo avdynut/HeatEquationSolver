91846b82edf84d3e7429d736d216d673ae1a8eaf
Andrei Arekhva <a.arekhva@gmail.com>
2016-09-27 21:49:12 +0300

    Add console project and implement Equation class

---
+++ ConsoleSolver/Program.cs
@@ -0,0 +1,28 @@
+﻿using HeatEquationSolver;
+
+namespace ConsoleSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eq = new Equation(u, k, g, (x, t) => 2 * x * t, (x, t) => 2 * t, (x, t) => x * x, (x, t, u) => 2 * u);
+            var d = eq.f(4, 3);
+        }
+
+        private static double u(double x, double t)
+        {
+            return x * x * t;
+        }
+
+        private static double k(double x, double t, double u)
+        {
+            return u * u;
+        }
+
+        private static double g(double x, double t, double u)
+        {
+            return x * x - 8 * x * x * t * t * u - 2 * t * u * u;
+        }
+    }
+}
--- HeatEquationSolver/Equation.cs
+++ HeatEquationSolver/Equation.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace HeatEquationSolver
 {
+    public delegate double Function(double x, double t);
+    public delegate double ComplexFunction(double x, double t, double u);
+
     public class Equation
     {
+        public Function f;
+
+        public Equation(Function u, ComplexFunction k, ComplexFunction g,
+            Function du_dx, Function d2u_dx, Function du_dt, ComplexFunction dk_du)
+        {
+            f = (x, t) => du_dt(x, t) - dk_du(x, t, u(x, t)) * Math.Pow(du_dx(x, t), 2) - k(x, t, u(x, t)) * d2u_dx(x, t) - g(x, t, u(x, t));
+        }
     }
 }
