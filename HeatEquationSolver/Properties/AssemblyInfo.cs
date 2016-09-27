b255be1eb2aca2b821d4d541bcf73b29bccc1799
Andrei Arekhva <a.arekhva@gmail.com>
2016-09-27 20:52:47 +0300

    Create empty class library project

---
+++ HeatEquationSolver/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HeatEquationSolver")]
+[assembly: AssemblyDescription("Solvign heat equations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AvdyNut")]
+[assembly: AssemblyProduct("HeatEquationSolver")]
+[assembly: AssemblyCopyright("Copyright © Andrei Arekhva 2017")]
+[assembly: AssemblyTrademark("AvdyNut")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd9c1151-54c7-4f86-9bad-0acb44cca252")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
