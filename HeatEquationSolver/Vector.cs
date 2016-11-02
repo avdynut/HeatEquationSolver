23cf1fb223004e0a3981808f43e71d70d9c6aea7
Andrei Arekhva <andrei_arekhva@epam.com>
2016-11-02 18:15:31 +0300

    Add image with initial and boundary conditions

--- HeatEquationSolver/Vector.cs
+++ HeatEquationSolver/Vector.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace HeatEquationSolver
 {
@@ -8,29 +7,6 @@ namespace HeatEquationSolver
         public static double[] MultiplyConst(double a, double[] vector)
         {
             return vector.Select(x => a * x).ToArray();
-        }
-
-        //public static double[] Add(double[] v, double[] u)
-        //{
-        //    if (v.Length != u.Length)
-        //        throw new ArgumentException("Lengths of vectors must match");
-
-        //    return v.Zip(u, (x, y) => x + y).ToArray();
-        //}
-
-        public static double[] GenerateRandom(double a, double b, int count)
-        {
-            if (a > b)
-                throw new ArgumentException("The left end must be less then right end of segment.");
-
-            Random r = new Random();
-            double[] x0 = new double[count];
-            double length = b - a;
-
-            for (int i = 0; i < count; i++)         // заполняем рандомно вектор первого приближения
-                x0[i] = a + length * r.NextDouble();
-
-            return x0;
         }
     }
 }
