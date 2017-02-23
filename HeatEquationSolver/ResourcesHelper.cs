36c0a57b1b374bc08013c4c3ef53aae6db793c9c
Andrei Arekhva <andrei_arekhva@epam.com>
2017-02-23 17:57:21 +0300

    Replace resources file with App.config, delete unnecessary files

--- HeatEquationSolver/ResourcesHelper.cs
+++ HeatEquationSolver/ResourcesHelper.cs
@@ -1,16 +1,21 @@
-﻿namespace HeatEquationSolver
+﻿using System.Configuration;
+
+namespace HeatEquationSolver
 {
-    public static class ResourcesHelper
-    {
-        public static string K => Properties.Resources.K;
-        public static string dK_du => Properties.Resources.dK_du;
-        public static string g => Properties.Resources.g;
-        public static string InitCond => Properties.Resources.InitCond;
-        public static string LeftBoundCond => Properties.Resources.LeftBoundCond;
-        public static string RightBoundCond => Properties.Resources.RightBoundCond;
-        public static string u => Properties.Resources.u;
-        public static string du_dx => Properties.Resources.du_dx;
-        public static string d2u_dx2 => Properties.Resources.d2u_dx2;
-        public static string du_dt => Properties.Resources.du_dt;
-    }
+	public static class ResourcesHelper
+	{
+		public static string K => ConfigurationManager.AppSettings[nameof(K)];
+		public static string dK_du => ConfigurationManager.AppSettings[nameof(dK_du)];
+		public static string g => ConfigurationManager.AppSettings[nameof(g)];
+		public static string InitCond => ConfigurationManager.AppSettings[nameof(InitCond)];
+		public static string LeftBoundCond => ConfigurationManager.AppSettings[nameof(LeftBoundCond)];
+		public static string RightBoundCond => ConfigurationManager.AppSettings[nameof(RightBoundCond)];
+		public static string u => ConfigurationManager.AppSettings[nameof(u)];
+		public static string du_dx => ConfigurationManager.AppSettings[nameof(du_dx)];
+		public static string d2u_dx2 => ConfigurationManager.AppSettings[nameof(d2u_dx2)];
+		public static string du_dt => ConfigurationManager.AppSettings[nameof(du_dt)];
+
+		public static double X1 = double.Parse(ConfigurationManager.AppSettings[nameof(X1)]);
+
+	}
 }
