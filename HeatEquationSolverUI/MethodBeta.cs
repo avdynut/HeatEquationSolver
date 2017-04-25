4ee8ca5d31ba67180168b3fd4202ddc2b1cc513c
Andrei Arekhva <a.arekhva@gmail.com>
2017-04-25 13:51:14 +0300

    Add pictures for methods, improve UI

--- HeatEquationSolverUI/MethodBeta.cs
+++ HeatEquationSolverUI/MethodBeta.cs
@@ -1,4 +1,5 @@
 ﻿using HeatEquationSolver.BetaCalculators;
+using System;
 
 namespace HeatEquationSolverUI
 {
@@ -6,21 +7,21 @@ namespace HeatEquationSolverUI
 	{
 		public BetaCalculator BetaCalculator { get; }
 		public string Name { get; }
-		public string RelativePathToImage { get; }
+		public string PathToImage { get; }
 
 		public MethodBeta(BetaCalculator betaCalculator, string name, string pictureFileName)
 		{
 			BetaCalculator = betaCalculator;
 			Name = name;
-			RelativePathToImage = $@"Images\MethodsForBeta\{pictureFileName}";
+			PathToImage = $@"{Environment.CurrentDirectory}\Images\MethodsForBeta\{pictureFileName}";
 		}
 
 		public static readonly MethodBeta[] Methods = {
 			new MethodBeta(BetaCalculator.Puzynin, "Метод И.В. Пузынина", "puzynin.PNG"),
-			new MethodBeta(BetaCalculator.Osmoip_1_5, "Одношаг. метод неполн. прогноза (1.5)", ""),
-			new MethodBeta(BetaCalculator.Osmoip_1_47, "Одношаг. метод неполн. прогноза (1.47)", ""),
-			new MethodBeta(BetaCalculator.Osmoip_1_267, "Одношаг. метод неполн. прогноза (1.267)", ""),
+			new MethodBeta(BetaCalculator.Osmoip_1_5, "Одношаг. метод неполн. прогноза (1.5)", "osmoip_1_5.PNG"),
+			new MethodBeta(BetaCalculator.Osmoip_1_47, "Одношаг. метод неполн. прогноза (1.47)", "osmoip_1_47.PNG"),
+			new MethodBeta(BetaCalculator.Osmoip_1_267, "Одношаг. метод неполн. прогноза (1.267)", "osmoip_1_267.PNG"),
 			new MethodBeta(BetaCalculator.Osmoip_1_274, "Одношаг. метод неполн. прогноза (1.274)", "osmoip_1_274.PNG"),
-			new MethodBeta(BetaCalculator.Osmoip_1_301, "Одношаг. метод неполн. прогноза (1.301)", "") };
+			new MethodBeta(BetaCalculator.Osmoip_1_301, "Одношаг. метод неполн. прогноза (1.301)", "osmoip_1_301.PNG") };
 	}
 }
